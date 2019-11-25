b05cce47e62b2249af78aa5477737f564eb44dfa
An Phan <antphan98@gmail.com>
2019-11-24 22:01:11 -0600

    added axios call and for loop for band info

--- liri.js
+++ liri.js
@@ -13,11 +13,27 @@ function findConcerts() {
     axios.get(`https://rest.bandsintown.com/artists/` + result + `/events?app_id=codingbootcamp`)
     .then(function(response) {
 
-        console.log(response);
+       for (let i = 0; i < response.data.length; i++) {
 
+        const date = response.data[i].date;
+        const dateArr = date.split(",");
 
+        const concertRes = "----------------------------------------" +
+        "\nVenue Name: " + response.data[i].venue.name;
+        "\nLocation: " + response.data[i].venue.city;
+      
+
+       }
+
+    
+
+    })
+    .catch(function(error) {
+        console.log(error);
 
     });
 
 
 }
+
+findConcerts();
