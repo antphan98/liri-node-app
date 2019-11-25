1a610c0748e2f1302400cbdf9dafdf2e0cdd8217
An Phan <antphan98@gmail.com>
2019-11-24 21:33:11 -0600

    added function for bands in town

--- liri.js
+++ liri.js
@@ -1,10 +1,23 @@
 require("dotenv").config();
 
 const keys = require("./keys.js");
-const spotify = new Spotify(keys.spotify);
+// const spotify = new Spotify(keys.spotify);
 
 const axios = require("axios");
 
 const userInput = process.argv[2];
 const result = process.argv[3];
 
+function findConcerts() {
+
+    axios.get(`https://rest.bandsintown.com/artists/` + result + `/events?app_id=codingbootcamp`)
+    .then(function(response) {
+
+        console.log(response);
+
+
+
+    });
+
+
+}
